8b45be7278ef6577beb862641bde66cba5d584f7
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-07-12 11:59:54 -0700

    Time: 0 ms (100%), Space: 15.6 MB (72.21%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void reverse(vector<int>& nums, int i){
+    void reverse(vector<int>& nums, int i){   //TC: O(n)  SC: O(1)
         int start = i;
         int end = nums.size()-1;
         while(start<end){
@@ -11,7 +11,7 @@ public:
     }    
     
     void nextPermutation(vector<int>& nums) {
-       int piv = -1;
+       int piv = -1;                       //finding pivot
        int n = nums.size();
 
        for(int i=n-2;i>=0;i--){
@@ -27,15 +27,16 @@ public:
        }
 
 
-       for(int i=n-1;i>=0;i--){
+       for(int i=n-1;i>=0;i--){            //next larger element
             if(nums[i]>nums[piv]){
                 swap(nums[i],nums[piv]);
                 break;
             }
        }
        
-       reverse(nums,piv+1);
+       reverse(nums,piv+1);         //reversing form pivot+1 to n-1
         
+       //reverse(A.begin()+pivot+1,A.end());
        
     }
 };
