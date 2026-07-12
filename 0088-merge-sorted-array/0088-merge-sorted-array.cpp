f9b4c136a72af56c25a74a16d598bdf008126e50
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-07-12 09:37:02 -0700

    Time: 0 ms (100%), Space: 12.4 MB (42.4%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = nums1.size()-1;
-        int j = nums2.size()-1;
+        int idx = m+n-1;
+        int i = m-1;
+        int j = n-1;
 
-        while(i>=m){
-            int temp = nums1[i];
-            nums1[i] = nums2[j];
-            nums2[j] = temp;
-            i--;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[idx] = nums1[i];
+                idx--;
+                i--;
+            }
+            else{
+                nums1[idx] = nums2[j];
+                idx--;
+                j--;
+            }
+        }
+        //it handles when i becomes 0 but j>0 as the elements of nums2 is not merged in nums1 but when i>0 in that case there is no need for extra condition as when j=0 but i>0 all the elements are already in its correct position.
+
+        while(j>=0){
+            nums1[idx] = nums2[j];
+            idx--;
             j--;
         }
-
-        sort(nums1.begin(),nums1.end());
-
         
     }
 };
