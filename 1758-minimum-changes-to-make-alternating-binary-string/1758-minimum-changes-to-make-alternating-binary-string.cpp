7387105a79302f18488e7372fb6b41343878cb6e
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-07-14 00:55:27 -0700

    Time: 0 ms (100%), Space: 9.6 MB (22.28%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int startWith0 = 0;
-        int startWith1 = 0;
+        int startswith0 = 0;
+        int startswith1 = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            // Pattern: 010101...
-            if (i % 2 == 0) {
-                if (s[i] != '0') startWith0++;
-            } else {
-                if (s[i] != '1') startWith0++;
+
+        for(int i=0;i<s.length();i++){
+            if(i%2==0){      //even index               //01010101010.... pattern 
+               if(s[i]!='0'){
+                startswith0++;
+               }
+            }
+            else{            //odd index
+                if(s[i]!='1'){
+                    startswith0++;
+                }
             }
 
-            // Pattern: 101010...
-            if (i % 2 == 0) {
-                if (s[i] != '1') startWith1++;
-            } else {
-                if (s[i] != '0') startWith1++;
+            if(i%2==0){    //even index                 //1010101010.... pattern 
+               if(s[i]!='1'){
+                startswith1++;
+               }
+            }
+            else{         //odd index
+                if(s[i]!='0'){
+                    startswith1++;
+                }
             }
         }
 
-        return min(startWith0, startWith1);
+        return min(startswith0,startswith1);
+        
     }
 };
