2d52e0f4cc12b7a4f51b9b3831c7d616a5323542
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-07-20 13:55:14 -0700

    Time: 3 ms (33.69%), Space: 9.3 MB (72.91%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(s.length()>0 && s.find(part)<s.length()){
-            s.erase(s.find(part),part.length());
+        while(s.length()>0 && s.find(part)<s.length()){     //when string is completely removed or substring is not found
+
+            s.erase(s.find(part),part.length());     // erases element from starting position of find function till length of substring found
         }
 
         return s;
