32997f1bfa3300caf6a5faffe5a6fcb528a2310d
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-07-22 06:36:27 -0700

    Time: 103 ms (5.59%), Space: 12.5 MB (79.72%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++){
             int d = 0;
             for(int j=i;j<nums.size();j++){
-                d = gcd(d,nums[j]);
+                d = gcd(d,nums[j]);       //calculate one subarray gcd and using previous subarray gcd to calculate next subarray gcd
                 if(d==k){
                     count++;
                 }
