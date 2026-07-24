06862f8743515a784bff4b8b4bfbce48dedc3718
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-07-24 06:54:58 -0700

    Time: 0 ms (100%), Space: 13.1 MB (98.87%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,11 +1,41 @@
 class Solution {
 public:
+    bool searchinrow(vector<vector<int>>& matrix, int target, int row){  //O(logn)
+        int n = matrix[0].size();
+        int st = 0, end = n-1;
+        while(st<=end){
+            int mid = st + (end - st)/2;
+            if(target == matrix[row][mid]){
+                return true;
+            }
+            else if(target>matrix[row][mid]){
+                st = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return false;
+
+    }
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+        int m = matrix.size(), n = matrix[0].size();
+        int sr = 0, endr = m - 1;
+
+        while(sr<=endr){   //O(logm)     TOTAL TC = O(log(m*n))
+            int midrow = sr + (endr - sr)/2;
+            if(target>=matrix[midrow][0] && target<=matrix[midrow][n-1]){
+                //found the row=> BS on this row
+                return searchinrow(matrix,target,midrow);
+            }
+            else if(target>=matrix[midrow][n-1]){
+                //down = right
+                sr = midrow + 1;
+            }
+            else {
+                //up = left
+                endr = midrow - 1;
             }
         }
         return false;
