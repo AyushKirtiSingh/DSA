206889116644d02cd89be1a05e38ab67fe04d3bc
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-07-26 05:15:59 -0700

    Time: 0 ms (100%), Space: 8.1 MB (79.56%) - LeetHub

--- 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.cpp
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int largestInteger(int n, int s) {
-        if(s>9*n){
+        if(s>9*n){       //largest possible consists only 9 and for n size the sum is 9*n and it should be less than s to be valid
             return -1;
         }
 
         string str = "";
 
         for(int i=0;i<n;i++){
-            int digit = min(9,s);
-            char ch = digit + '0';
+            int digit = min(9,s);     // placing the largets possible first digit
+            char ch = digit + '0';    //converting to string na dcharacter to concatenate numbers
             str += ch;
-            s -= digit;
+            s -= digit;     //decreasing s by digit to cal culate next possible largest digit
             
         }
 
