6150fda1fcbf42f752fecea113f64896ff0d0db1
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-07-27 06:06:23 -0700

    Time: 23 ms (54.02%), Space: 17.3 MB (84.94%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,14 +4,14 @@ public:
         int n = nums.size();
         long long sum = 0;
         vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());   //0(nlogn)
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){     //O(n)
             if(i>0 && nums[i]==nums[i-1])continue;
-            for(int j=i+1;j<n;){
+            for(int j=i+1;j<n;){     //O(n)
                 
                 int p = j+1, q = n-1;
-                while(p<q){
+                while(p<q){    //O(n)
                     sum = (long long)nums[i]+(long long)nums[j]+(long long)nums[p]+(long long)nums[q];
                     if(sum<target){
                         p++;
@@ -27,7 +27,8 @@ public:
                         }
                     }
                     
-                }
+                }                      //T.C : O(nlogn + n^3)
+                                       //S.C : O(unique groups)
 
                 j++;
                 while(j<n && nums[j]==nums[j-1])j++;
