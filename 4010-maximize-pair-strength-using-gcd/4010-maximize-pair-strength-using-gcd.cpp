cd68a39738c119349a252833ced9d0059eb18461
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-02 01:29:30 -0700

    Time: 750 ms (68.93%), Space: 36.9 MB (86.19%) - LeetHub

--- 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.cpp
+++ 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.cpp
@@ -7,7 +7,7 @@ public:
             for(long long j=i+1;j<nums.size();j++){
                 long long currpair = 0;
                 long long gcd = std::gcd(nums[i],nums[j]);
-                currpair = (1LL*nums[i]*nums[j])/pow(gcd,2);
+                currpair = (1LL*nums[i]*nums[j])/pow(gcd,2);  //"The calculation itself is overflowing its current datatype, not necessarily the variable I'm storing the result in.So thats why i am using 1LL"
                 maxpair = max(maxpair,currpair);
 
             }
