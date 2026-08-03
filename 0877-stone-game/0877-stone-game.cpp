c1cdac26a4b1d66760c0ec456c72e139692f2f3a
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-03 13:49:56 -0700

    Time: 0 ms (100%), Space: 10.4 MB (86.99%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        return true;    //alice hamesha jeetegi kyunki wo hamesha bda element element legi either from start or bottom and right and left keep on dec and inc until there are no more piles left. alice bda legi issiliye hamesha jeetegi bob se aur bob hamesha haarega
         
     }
 };
