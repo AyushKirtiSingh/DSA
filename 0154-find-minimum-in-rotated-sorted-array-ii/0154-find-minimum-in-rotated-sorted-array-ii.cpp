efc821b14691f56ba0a9f16b5e6e9d237a286080
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-04 14:52:27 -0700

    Time: 0 ms (100%), Space: 16.3 MB (29.19%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int min = nums[0];
-        for(int i=0;i<nums.size();i++){
-            if(min>nums[i]){
-                min = nums[i];
+        int n = nums.size();
+        int st = 0, end = n-1;
+
+        if(nums[0]<nums[n-1]){ //check karta hai rotation huya hai ya nhi
+            return nums[0];
+        }
+
+        while(st<end){
+            int mid = st + (end - st)/2;
+            if(nums[mid]==nums[end]){  //if min is less than mid and it contains duplicate vlues in arrays end-- will be safe
+                end--;
+            }
+
+            else if(nums[mid]<nums[end]){
+                end = mid;    ////jab mid chota hoga tab left side mein ya phir mid hi minimum ho skta h issiliye mid = end
+            }
+            else{
+                st = mid + 1;   //jab mid bda hoga matlab min right side mein hoga
             }
         }
-        return min;
+        return nums[end];  //because condition tab fail hoti h jab st==end hota h toh return st ya end koi bhi index ki value ko kar skte h kyuki end mein 1 hi element aayega
+        
     }
 };
