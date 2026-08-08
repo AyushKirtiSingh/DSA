c12c5d9125f11edca5098b17eeb091e918449529
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-08 09:02:12 -0700

    Time: 91 ms (5.67%), Space: 64.3 MB (5.17%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
-    long long rev(long long n){
+
+    // Number ko reverse karta hai
+    long long rev(long long n) {
         string ans = "";
         string str = to_string(n);
-        for(int i=str.length()-1;i>=0;i--){
+
+        for(int i = str.length() - 1; i >= 0; i--) {
             ans += str[i];
-                
         }
+
         return stoll(ans);
     }
-    
-    
+
+
     int countNicePairs(vector<int>& nums) {
         const long long MOD = 1000000007;
+
         long long count = 0;
-        for(int i=0;i<nums.size();i++){
+
+        // nums[i] ko nums[i] - rev(nums[i]) mein transform karo
+        for(int i = 0; i < nums.size(); i++) {
             nums[i] = nums[i] - rev(nums[i]);
         }
-      
-        unordered_map<int,int> m;
 
-        for(int x: nums){
-            if(m.find(x)==m.end()){
+        // Har transformed value ki frequency store karega
+        unordered_map<int, int> m;
+
+        for(int x : nums) {
+
+            // Agar x pehli baar aaya hai
+            if(m.find(x) == m.end()) {
                 m[x] = 1;
             }
-            else{
+
+            else {
+                // Jitni baar x pehle aaya, utne new pairs banenge
                 count += m[x];
+
+                // Current x ko bhi frequency mein add karo
                 m[x]++;
             }
         }
-        return (count % MOD);
-        
-        
+
+        // Answer ko MOD ke according return karo
+        return count % MOD;
     }
 };
