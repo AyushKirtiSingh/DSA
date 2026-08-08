19139a7779164287d298818084d35a8ac8d10583
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-08 07:10:17 -0700

    Time: 44 ms (59.86%), Space: 57.7 MB (35.54%) - LeetHub

--- 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.cpp
+++ 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.cpp
@@ -11,12 +11,12 @@ public:
             while(i<s.length() && s[i] != ' '){
                 words += s[i];
                 i++;
-            }
+            }                 //the main problem i got stuck was to find the first word for that took another variable j to check that and initialized before for loop so it never resets.
             j++;
             if(j==0){
                 for(int i=0;i<words.length();i++){
                     if(words[i]=='a' || words[i]=='e' || words[i]=='i' || words[i]=='o' || words[i]=='u'){
-                        count++;
+                        count++;   //first word vowel count and stored forevers not resets for different i.
 
                     }
                 }
@@ -24,20 +24,20 @@ public:
             else{
                 for(int i=0;i<words.length();i++){
                     if(words[i]=='a' || words[i]=='e' || words[i]=='i' || words[i]=='o' || words[i]=='u'){
-                        count1++;
+                        count1++;   //count of vowels of other words and it always resets for each value of i
                         
                     }
                     
 
                 }
-                if(count1==count){
+                if(count1==count){   //comparing the vowel count na dperforming operation
                         reverse(words.begin(),words.end());
                 }
             }
-            ans += ' ' + words;
+            ans += ' ' + words;   //after the legal operation adding in new string with a space
 
         }
-        return ans.substr(1);
+        return ans.substr(1);   //to remove the initial space in the ans
         
     }
 };
