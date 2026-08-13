6108175b261162ac6227c891e4d0e9e5665bf4cd
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-13 21:19:31 +0530

    Time: 172 ms (55.37%), Space: 99.9 MB (85.78%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -4,9 +4,9 @@ public:
         int maxpairsum = 0;
         sort(nums.begin(),nums.end());
 
-        int i=0, j=nums.size()-1;
+        int i=0, j=nums.size()-1;   //intuition is to minimize the pairsum pair the largest with the smallest element left in nums
 
-        while(i<j){
+        while(i<j){     
             int sum = nums[i] + nums[j];
             maxpairsum = max(maxpairsum,sum);
             i++, j--;
