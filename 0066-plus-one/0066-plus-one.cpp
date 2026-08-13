2d650f21c72dc7f918c6ef8b0c7a07b6d54c21ff
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-13 12:07:35 +0530

    Time: 0 ms (100%), Space: 11.4 MB (86.99%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        
-        for(int i=digits.size()-1;i>=0;i--){
-            if(digits[i]!=9){
+
+        // Last digit se start karenge, kyunki carry right se aata hai
+        for(int i = digits.size() - 1; i >= 0; i--){
+
+            // Agar digit 9 nahi hai, simply +1 karo
+            if(digits[i] != 9){
                 digits[i]++;
                 break;
+            }
 
-            }
+            // Agar digit 9 hai, toh 0 karo aur carry left jayega
             else{
-                digits[i]=0;
+                digits[i] = 0;
             }
         }
-        if(digits[0]==0){
-            digits.insert(digits.begin(),1);
+
+        // Agar first digit bhi 0 ho gaya,
+        // iska matlab saare digits 9 the
+        // Example: 999 -> 000, so beginning mein 1 add karo
+        if(digits[0] == 0){
+            digits.insert(digits.begin(), 1);
         }
+
         return digits;
-        
     }
 };
