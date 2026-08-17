6b96ef8bd7852115500abd5cd059e2e95f84490a
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-17 09:49:57 +0530

    Time: 0 ms (100%), Space: 14.9 MB (93.75%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,35 +11,50 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        // Pehle linked list ka size find kar rahe hain
         int size = 0;
         ListNode* curr = head;
 
-        while(curr != NULL){
+        while(curr != NULL) {
             curr = curr->next;
             size++;
         }
-        if(size==1){
+
+        // Agar sirf ek node hai, toh wahi remove hogi
+        if(size == 1) {
             return NULL;
         }
-        int idx = size - n;
-        if(idx==0)return head->next;
 
+        // Nth node from end ka index from beginning
+        int idx = size - n;
+
+        // Agar first node remove karni hai
+        if(idx == 0)
+            return head->next;
+
+        // prev = delete hone wale node se pehle wala node
+        // temp = delete hone wala node
         int i = 1;
         ListNode* temp = head->next;
         ListNode* prev = head;
-        while(i!=idx){
+
+        // Required position tak pointers move karo
+        while(i != idx) {
             i++;
             prev = prev->next;
             temp = temp->next;
-            
         }
 
+        // Node ko list se remove karo
         prev->next = prev->next->next;
+
+        // Removed node ko safely disconnect karo
         temp->next = NULL;
+
+        // Memory free karo
         delete temp;
 
         return head;
-
-        
     }
 };
