da66f6fc300d223e2ede4fd93d567d60011cdab4
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-19 00:06:55 +0530

    Time: 3 ms (62.95%), Space: 18.3 MB (26.1%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -3,6 +3,7 @@ public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         vector<string> ans;
         unordered_map<string,int> m;
+        // har word ki frequency count karo
         for(int i=0;i<words.size();i++){
             if(m.find(words[i])==m.end()){
                 m[words[i]] = 1;
@@ -15,10 +16,12 @@ public:
 
         vector<pair<int,string>> v;
 
+        // frequency aur word ko pair me store karo
         for(auto it : m){
             v.push_back({it.second,it.first});
         }
 
+        // pehle high frequency, same frequency par lexicographically small word
         sort(v.begin(),v.end(),[](pair<int,string> a,pair<int,string> b){
         if(a.first != b.first){
             return a.first > b.first;
@@ -26,6 +29,7 @@ public:
         return a.second < b.second;
     });
 
+        // top k frequent words answer me daalo
         for(int i=0;i<k;i++){
             ans.push_back(v[i].second);
         }
