f728d3ebf1a42e95791fb591ab263cf9329c092d
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-26 19:10:38 +0530

    Time: 41 ms (24.59%), Space: 97.2 MB (5.73%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,34 +1,40 @@
 class StockSpanner {
 public:
-    stack<pair<int,int>> st;
+    stack<pair<int,int>> st; 
+    // pair = {price, index}; stack class ke andar hai taaki data next() calls ke beech preserve rahe
+
     int ans = 0;
-    int idx = 0;
+    int idx = 0; 
+    // idx = current price ka index
+
     StockSpanner() {
-        
+        // Constructor: object create hote time ek baar call hota hai
     }
     
     int next(int price) {
+        // Har new price ke liye Stock Span calculate hota hai
         
         while(st.size()>0 && st.top().first<=price){
+            // Top ka price <= current price → useless, remove
             st.pop();
         }
 
         if(st.size()==0){
+            // Koi previous greater price nahi mila
             ans = idx + 1;
         }
         else{
+            // Top = previous greater price; second = uska index
             ans = idx - st.top().second;
         }
 
         st.push({price,idx});
+        // Current price + index future calls ke liye save
+
         idx++;
+        // Next price ke liye index increase
 
         return ans;
+        // Current Stock Span return
     }
 };
-
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner* obj = new StockSpanner();
- * int param_1 = obj->next(price);
- */
