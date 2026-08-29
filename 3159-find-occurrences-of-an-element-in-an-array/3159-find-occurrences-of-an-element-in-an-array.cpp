db4ddfe8342d4aaae19cf77c77fc7bcfbff17900
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-29 15:03:53 +0530

    Time: 54 ms (8.54%), Space: 192.8 MB (7.78%) - LeetHub

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
@@ -3,6 +3,8 @@ public:
     vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
         unordered_map<int,int> m;
         vector<int> ans;
+
+        // Count the total occurrences of each element.
         for(int v : nums){
             if(m.find(v)==m.end()){
                 m[v] = 1;
@@ -11,34 +13,34 @@ public:
                 m[v]++;
             }
         }
+
+        // Store the index of each occurrence of x.
         unordered_map<int,int> q;
-            int count = 0;
-            int j = 0;
-            while(j<nums.size()){
-                if(nums[j]==x){
-                    count++;
-                    q[count] = j;
-                }
-                j++;
+        int count = 0;
+        int j = 0;
+
+        while(j<nums.size()){
+            if(nums[j]==x){
+                count++;
+                q[count] = j;
             }
-            
+            j++;
+        }
+
+        // Answer each query using the stored occurrence indices.
         for(int i=0;i<queries.size();i++){
 
+            // If the requested occurrence does not exist.
             if(queries[i]>m[x]){
                 ans.push_back(-1);
             }
             else{
-                if(q.find(queries[i])==q.end()){
-                    ans.push_back(-1);
-                }
-                else{
-                    ans.push_back(q[queries[i]]);
-                }
+                // Return the index of the requested occurrence.
+                ans.push_back(q[queries[i]]);
             }
-            
         }
+
         return ans;
-
-
     }
 };
+
