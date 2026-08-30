01320a0c89e2310c733e2bf04f94f7b0b4e93cfd
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-30 19:40:18 +0530

    Time: 2 ms (20.09%), Space: 20.8 MB (75.92%) - LeetHub

--- 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
@@ -4,14 +4,17 @@ public:
         int count = 0;
         unordered_map<int,int> m;
 
+        // First element always starts the first block
         m[nums[0]] = 1;
 
         for(int i=1;i<nums.size();i++){
+            // A new block starts whenever the current value differs from the previous one
             if(nums[i]!=nums[i-1]){
                 m[nums[i]]++;
             }
         }
 
+        // An integer is special if all its occurrences form exactly one block
         for(auto it : m){
             if(it.second==1){
                 count++;
