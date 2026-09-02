e9a795f86930695190df859e45fbfcaee3d0bd59
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-02 18:37:43 +0530

    Time: 0 ms (100%), Space: 25.9 MB (93.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {   //T.C = O(n) && S.C = O(n)
-        int n = height.size();
+    int trap(vector<int>& height) {
+        int n = height.size();        //TC : O(N) && SC : O(1)
         int ans = 0;
-        vector<int> lmax(n,0);
-        vector<int> rmax(n,0);     //Prefix array approach
+      
+        int l=0, r=n-1;
+        int lmax = 0, rmax = 0;      // TWO POINTERS
 
-        lmax[0] = height[0];
-        rmax[n-1] = height[n-1];
+        while(l<r){
+            lmax = max(lmax,height[l]);
+            rmax = max(rmax,height[r]);
 
-        for(int i=1;i<n;i++){
-            lmax[i] = max(lmax[i-1],height[i]);
+            if(lmax<rmax){
+                ans += lmax - height[l];
+                l++;
+            }
+            else{
+                ans += rmax - height[r];
+                r--;
+            }
         }
-
-        for(int i=n-2;i>=0;i--){
-            rmax[i] = max(rmax[i+1],height[i]);
-        }
-
-        for(int i=0;i<n;i++){
-            ans += max(0,min(rmax[i],lmax[i]) - height[i]);
-        }
-
         return ans;
     }
 };
