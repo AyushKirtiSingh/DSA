3b842ff2a35c3c44dddcdb3fb1d0d0f5dbfe61cf
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-06 18:04:17 +0530

    Time: 0 ms (100%), Space: 103.1 MB (85.24%) - LeetHub

--- 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.cpp
+++ 4044-count-good-cyclic-rotations/4044-count-good-cyclic-rotations.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     int countGoodRotations(vector<int>& nums) {
         int n = nums.size();
-        long long total = 0;
+        long long sum2 = 0;
 
         int half = n/2;
 
-        for(int x : nums){
-            total += x;
-        }
-
-        long long sum = 0;
+        long long sum1 = 0;
 
         for(int i=0;i<half;i++){
-            sum += nums[i];
+            sum1 += nums[i];
+        }
+
+        for(int i=half;i<n;i++){
+            sum2 += nums[i];
         }
 
         int result = 0;
 
         for(int i=0;i<n;i++){
-            if(sum>total-sum){
+            if(sum1>sum2){
                 result++;
             }
 
-            sum -= nums[i];
-            sum += nums[(i+half)%n];
+            sum1 -= nums[i];
+            sum1 += nums[(i+half)%n];
+
+            sum2 -= nums[(half+i) % n];
+            sum2 += nums[i];
         }
 
         return result;
