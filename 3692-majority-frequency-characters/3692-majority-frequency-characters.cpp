c50c17cfed53e6835e0ab4b9857fb2f2956ed4fa
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-08 01:58:31 +0530

    Time: 3 ms (62.3%), Space: 11 MB (8.52%) - LeetHub

--- 3692-majority-frequency-characters/3692-majority-frequency-characters.cpp
+++ 3692-majority-frequency-characters/3692-majority-frequency-characters.cpp
@@ -4,6 +4,7 @@ public:
         unordered_map<char,int> m;
         string ans = "";
 
+        // Har character ki frequency count kar rahe hain
         for(char x : s){
             if(m.find(x)==m.end()){
                 m[x] = 1;
@@ -14,6 +15,7 @@ public:
         }
         unordered_map<int,int> q;
 
+        // Har frequency kitne characters ki hai, wo count kar rahe hain
         for(auto it : m){
             if(q.find(it.second)==q.end()){
                 q[it.second]=1;
@@ -26,6 +28,8 @@ public:
         int maxi = 0;
         int freq = 0;
 
+        // Maximum characters wali frequency find kar rahe hain
+        // Tie ho toh larger frequency choose karenge
         for(auto it : q){
             if(it.second > maxi || (it.second == maxi && it.first > freq)){
                 maxi = it.second;
@@ -35,10 +39,12 @@ public:
 
         set<char> str;
 
+        // Unique characters ko set mein store kar rahe hain
         for(char x : s){
             str.insert(x);
         }
 
+        // Sirf winning frequency wale characters answer mein add kar rahe hain
         for(char x : str){
             if(m[x]==freq){
                 ans += x;
@@ -48,3 +54,4 @@ public:
         return ans;
     }
 };
+
