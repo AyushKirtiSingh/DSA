f48e70d1f8e316b1902c0a5717734e47252a9498
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-08 01:02:31 +0530

    Time: 193 ms (24.42%), Space: 289.5 MB (79.38%) - LeetHub

--- 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.cpp
+++ 3843-first-element-with-unique-frequency/3843-first-element-with-unique-frequency.cpp
@@ -3,6 +3,7 @@ public:
     int firstUniqueFreq(vector<int>& nums) {
         unordered_map<int,int> m;
 
+        // Har element ki frequency count kar rahe hain
         for(int x : nums){
             if(m.find(x)==m.end()){
                 m[x] = 1;
@@ -14,6 +15,7 @@ public:
 
         unordered_map<int,int> f;
 
+        // Har frequency kitne elements ki hai, wo count kar rahe hain
         for(auto it : m){
             if(f.find(it.second)==f.end()){
                 f[it.second] = 1;
@@ -23,12 +25,16 @@ public:
             }
         }
 
+        // Original order mein pehla element check kar rahe hain
+        // jiska frequency sirf ek element ke paas hai
         for(int i=0;i<nums.size();i++){
             if(f[m[nums[i]]]==1){
                 return nums[i];
             }
         }
 
+        // Agar koi valid element nahi mila
         return -1;
     }
 };
+
