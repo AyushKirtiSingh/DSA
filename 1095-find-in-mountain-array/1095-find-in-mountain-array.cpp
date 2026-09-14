45310eb17f30cc9e056e7a9933126abf3a5b6965
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-14 16:51:56 +0530

    Time: 4 ms (74.07%), Space: 10 MB (38.44%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -14,74 +14,81 @@ public:
         int n = mountainArr.length();
         int peak = 0;
         int st = 0;
-        int end = n-2;
+        int end = n - 2;
         int ans = INT_MAX;
 
-        while(st<=end){
-            int mid = st + (end-st)/2;
-            if(mid!=0){
-                int l = mountainArr.get(mid-1);
-            int r = mountainArr.get(mid+1);
-            int m = mountainArr.get(mid);
-            if(l<m && m>r){
-                peak = mid;
-                break;
-            }
-            else if(l<m){
-                st = mid + 1;
-            }
-            else{
-                end = mid - 1;
-            }
+        // Peak index find kar rahe hain using binary search
+        while(st <= end) {
+            int mid = st + (end - st) / 2;
 
+            if(mid != 0) {
+                int l = mountainArr.get(mid - 1);
+                int r = mountainArr.get(mid + 1);
+                int m = mountainArr.get(mid);
+
+                // Mid peak hai
+                if(l < m && m > r) {
+                    peak = mid;
+                    break;
+                }
+                // Increasing side par hain, right jao
+                else if(l < m) {
+                    st = mid + 1;
+                }
+                // Decreasing side par hain, left jao
+                else {
+                    end = mid - 1;
+                }
             }
-            else{
+            else {
                 st++;
             }
-            
         }
 
-        st = 0, end = peak;
+        st = 0;
+        end = peak;
 
-        while(st<=end && ans==INT_MAX){
-            int mid = st + (end-st)/2;
+        // Increasing part mein binary search
+        while(st <= end && ans == INT_MAX) {
+            int mid = st + (end - st) / 2;
             int m = mountainArr.get(mid);
 
-            if(m==target){
-                ans = min(ans,mid);
+            if(m == target) {
+                ans = min(ans, mid);
             }
-            else if(m<target){
+            else if(m < target) {
                 st = mid + 1;
             }
-            else{
+            else {
                 end = mid - 1;
             }
         }
 
-        st = peak, end=n-1;
+        st = peak;
+        end = n - 1;
 
-        while(st<=end && ans==INT_MAX){
-            int mid = st + (end-st)/2;
+        // Decreasing part mein binary search
+        while(st <= end && ans == INT_MAX) {
+            int mid = st + (end - st) / 2;
             int m = mountainArr.get(mid);
 
-            if(m==target){
-                ans = min(ans,mid);
+            if(m == target) {
+                ans = min(ans, mid);
             }
-            else if(m>target){
+            // Decreasing order hai, target right mein hoga
+            else if(m > target) {
                 st = mid + 1;
             }
-            else{
-                end = mid -1;
+            else {
+                end = mid - 1;
             }
         }
 
-        if(ans==INT_MAX){
+        if(ans == INT_MAX) {
             return -1;
         }
-        else{
+        else {
             return ans;
         }
-
-        
     }
 };
