a504ae91f76a508efe7c53e21d9dc63a840c5a26
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-21 18:17:15 +0530

    Time: 5 ms (48.15%), Space: 74 MB (37.16%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> diff(n+2,0);  //to avoid the edge case index of end +1 we take size n+2
+
+        // n+2 size rakha hai taaki end+1 index safely handle ho sake
+        vector<int> diff(n+2,0);
 
         for(auto &b : bookings){
-            int start = b[0];      // difference array technique standard approach
-                                   // line sweep technique
+
+            // Booking ka starting aur ending flight
+            int start = b[0];
             int end = b[1];
+
+            // Har flight ke liye reserved seats
             int count = b[2];
 
-
+            // start se seats add karna start hongi
             diff[start] += count;
+
+            // end ke baad seats hat jayengi
             diff[end+1] -= count;
         }
 
-        //result store commulative sum
+        // Prefix sum se har flight ka actual total seats niklega
 
         vector<int> result;
         int currsum = 0;
+
         for(int i=1;i<=n;i++){
+
+            // Ab tak ke saare active bookings ka total
             currsum += diff[i];
+
+            // Current flight ka total seats store karo
             result.push_back(currsum);
         }
 
