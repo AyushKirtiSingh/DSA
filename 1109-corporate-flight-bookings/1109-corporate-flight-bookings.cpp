b6c7c1fe71427ea3c231abb77ec7ebe07f996074
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-21 18:10:38 +0530

    Time: 1 ms (65.84%), Space: 74 MB (34.35%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> diff(n+2,0);  //to avoid the edge case index of end +1 we take size n+2
+
+        for(auto &b : bookings){
+            int start = b[0];      // difference array technique standard approach
+            int end = b[1];
+            int count = b[2];
+
+
+            diff[start] += count;
+            diff[end+1] -= count;
+        }
+
+        //result store commulative sum
+
+        vector<int> result;
+        int currsum = 0;
+        for(int i=1;i<=n;i++){
+            currsum += diff[i];
+            result.push_back(currsum);
+        }
+
+        return result;
+    }
+};
