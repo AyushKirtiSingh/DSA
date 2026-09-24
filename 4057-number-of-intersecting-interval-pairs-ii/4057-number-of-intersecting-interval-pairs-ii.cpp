c591555e5822c30ef7a9bcbd6c17873d269b7f28
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-25 01:51:29 +0530

    Time: 120 ms (67.98%), Space: 258.8 MB (68.94%) - LeetHub

--- 4057-number-of-intersecting-interval-pairs-ii/4057-number-of-intersecting-interval-pairs-ii.cpp
+++ 4057-number-of-intersecting-interval-pairs-ii/4057-number-of-intersecting-interval-pairs-ii.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     long long countIntersectingIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
 
-        priority_queue<int,vector<int>,greater <int>> pq;
+        // Starting point ke according intervals sort karo
+        sort(intervals.begin(), intervals.end());
+
+        // Previous active intervals ke END points store honge
+        // Min-heap: sabse chhota end sabse upar
+        priority_queue<int, vector<int>, greater<int>> pq;
 
         long long ans = 0;
 
-        for(int i=0;i<intervals.size();i++){
-            while(pq.size()!=0 && pq.top()<intervals[i][0]){
-                pq.pop();
-            }
+        for(auto &x : intervals) {
 
+            // Jo intervals current interval ke start
+            // hone se pehle khatam ho chuke hain,
+            // unko remove karo
+            while(!pq.empty() && pq.top() < x[0])
+                pq.pop();
+
+            // Heap mein jitne intervals bache hain,
+            // woh current interval ke saath intersect karenge
             ans += pq.size();
 
-            pq.push(intervals[i][1]);
+            // Current interval ka end future ke liye store karo
+            pq.push(x[1]);
         }
 
         return ans;
-        
     }
 };
